16414465def835d40717290cdf41cae8e95d43b0
jojimer <jojimerc@gmail.com>
2020-07-01 00:41:58 +0800

    add jquery from other source

--- footer.php
+++ footer.php
@@ -4,11 +4,10 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="/includes/js/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script type="text/javascript" src="/js/navigation.js"></script>
-    <script type="text/javascript" src="/js/router.js"></script>
     <script type="text/javascript">
           jQuery(document).ready(function($){
             <?php redirectToPages(); ?>
